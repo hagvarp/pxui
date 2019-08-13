d4b6c21d6bb13018c6add22b15274b529a8c5993
Helgi Poulsen <helgipoulsen@hotmail.com>
2019-08-13 13:57:57 +0100

    ok

--- src/components/ListMenu/ListMenu.jsx
+++ src/components/ListMenu/ListMenu.jsx
@@ -1,63 +1,53 @@
 import React, { useState, useEffect } from "react";
-import { useHttp } from "../../hooks/http";
 import "./ListMenu.css";
-import ListItem from "./ListItem";
+import { resolve } from "uri-js";
 
+let test = [];
+let url = "https://statbank.hagstova.fo/api/v1/fo/H2/";
+let t = null;
 const ListMenu = () => {
-  const [isLoading, fetchedData] = useHttp(
-    "http://statbank.hagstova.fo/api/v1/fo/H2",
-    []
-  );
-  const [getData, setGetData] = useState(null);
-  let test = [];
-
-  const handleChange = e => {
-    fetchGetHttp(e);
-  };
-  //useEffect
-  if (fetchedData) {
-    for (let i = 0; i < fetchedData.length; i++) {
-      let id = fetchedData[i].id;
-      let text = fetchedData[i].text;
-      test.push(
-        <ListItem
-          className="menu-item"
-          text={text}
-          id={id}
-          callBack={handleChange}
-        >
-          {text}
-        </ListItem>
-      );
-    }
-  }
-  const fetchGetHttp = async id => {
-    await fetch("https://statbank.hagstova.fo/api/v1/fo/H2/" + id)
+  useEffect(() => {
+    fetch(url)
       .then(response => {
         if (!response.ok) {
           throw new Error("Failed to fetch.");
         }
-        return response.text();
+        return response.json();
       })
-      .then(response => {
-        console.log(response);
-        setGetData(response);
+      .then(data => {
+        for (let i = 0; i < data.length; i++) {
+          let id = data[i].id;
+          let type = data[i].type;
+          let text = data[i].text;
+          let mainOjb = { id, type, text };
+
+          if (type == "l") {
+            let tmpUrl = url + id + "/";
+            fetch(tmpUrl)
+              .then(response => {
+                if (!response.ok) {
+                  throw new Error("Failed to fetch.");
+                }
+                return response.json();
+              })
+              .then(response => {
+                test.push({ mainOjb, response });
+                console.log(test);                
+                //tmpUrl = tmpUrl + id + "/";
+              });
+          }
+        }
       })
       .catch(err => {
         console.log(err);
       });
-  };
-
-  if (isLoading) {
-    return <div>LOADING...</div>;
-  }
+  }, []);
 
   return (
     <div>
       <div style={{ fontWeight: "bold", fontSize: "1.5em", color: "#2d4182" }}>
         Hagtalsgrunnur
       </div>
-      {test}
     </div>
   );
 };
