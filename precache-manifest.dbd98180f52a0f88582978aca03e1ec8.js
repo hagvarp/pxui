e3091909be07de98563e8012265267b64fd4b3d4
Helgi Poulsen <helgipoulsen@hotmail.com>
2019-09-27 14:47:48 +0100

    Updates

---
+++ precache-manifest.dbd98180f52a0f88582978aca03e1ec8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f1535d45a2ad30edd83cfb382083d8",
+    "url": "/pxui/index.html"
+  },
+  {
+    "revision": "a8f53635dede44d09e10",
+    "url": "/pxui/static/css/2.3aa4d2b7.chunk.css"
+  },
+  {
+    "revision": "4b6bae1940b219a5fd7b",
+    "url": "/pxui/static/css/main.529f6b1a.chunk.css"
+  },
+  {
+    "revision": "a8f53635dede44d09e10",
+    "url": "/pxui/static/js/2.5014b93c.chunk.js"
+  },
+  {
+    "revision": "4b6bae1940b219a5fd7b",
+    "url": "/pxui/static/js/main.75d91ce1.chunk.js"
+  },
+  {
+    "revision": "9dfee86e7a3161578646",
+    "url": "/pxui/static/js/runtime~main.1114999b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/pxui/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/pxui/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/pxui/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/pxui/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/pxui/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "565f9a196fcb9823f67f3a9f01e6f2bd",
+    "url": "/pxui/static/media/logo.565f9a19.svg"
+  }
+]);
