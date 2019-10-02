0dd73358fd6657772583e990a9bb64d6e07b5b82
Helgi Poulsen <helgipoulsen@hotmail.com>
2019-10-02 15:48:52 +0100

    asd

--- src/components/Layouts/MainBody.jsx
+++ src/components/Layouts/MainBody.jsx
@@ -5,7 +5,6 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 
 import Selectors from "../Select/Selectors";
-import "./Index.css";
 import ListMenu from "../ListMenu/ListMenu";
 import TableData from "../TablesData/TablesData";
 
