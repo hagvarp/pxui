92e4d70c042df4f2eb8fa738caa0bdd302f0de5d
Helgi Poulsen <helgipoulsen@hotmail.com>
2019-11-14 01:12:23 +0000

    idk

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "../src/styles/style.css";
 import Main from "./components/Layouts/Main";
-import { HashRouter } from 'react-router-dom'
 
 function App() {
 
 
   return (
     <div className="app" >
-      <HashRouter><Main ></Main></HashRouter>
+      <Main ></Main>
 
     </div>
   );
--- src/components/Layouts/Main.jsx
+++ src/components/Layouts/Main.jsx
@@ -33,7 +33,7 @@ import StatisticIceland from "../../img/iceland.png";
 import SpecificDBSelecor from "../DbSelector/SpecificDBSelecor";
 import About from "./About";
 
-import { BrowserRouter, Route, Switch,HashRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch, HashRouter } from "react-router-dom";
 let urlForSpecificDB = "";
 
 export default function MainBody() {
@@ -376,7 +376,7 @@ export default function MainBody() {
           <Route
             excat
             path="/"
-            render={props => (
+            render={() => (
               <Grow
                 in={checked}
                 style={{ transformOrigin: "0 0 0" }}
